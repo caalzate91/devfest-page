2ad0862035a805fc2d8afc0f9f2a8676e39e8e32
Stiven <stivencastillo.90@gmail.com>
2023-09-10 13:10:26 -0500

    Add animate.css

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,7 +7,7 @@ const Counter = ({ targetDate }: Props) => {
   const { days, hours, minutes, seconds } = useCountdown(targetDate)
 
   return (
-    <div className="h-16 grow py-4 px-8 gap-1 bg-black text-white flex items-center">
+    <div className="h-16 grow py-4 px-8 gap-1 bg-black text-white flex items-center animate__animated animate__fadeIn">
       <span>{days}d</span>:<span>{hours}h</span>:<span>{minutes}m</span>:
       <span>{seconds}s</span>
     </div>
