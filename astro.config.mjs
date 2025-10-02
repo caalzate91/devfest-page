18ddb7861c04a04c2852fa263d6288860a147e26
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2025-10-01 21:25:48 -0500

    chore!: migrar proyecto a Astro 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://devfest.gdgcali.dev/",
-  integrations: [tailwind(), react()],
+  integrations: [ react()],
+
   build:{
     rollupOptions:{
       external:[]
     }
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
   }
 });
