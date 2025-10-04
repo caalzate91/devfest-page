dcce78811cc37f862a98f9a16508f0ecafe0c4f5
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2025-10-03 23:50:14 -0500

    feat: terminar diseño sección Hero
    Temporalmente se ha desactivado el atributo "base" del astro.config.mjs para lograr visualizar los recursos de la carpeta public

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://caalzate91.github.io",
-  base: "/devfest-page/",
+  base: "/",
   integrations: [react()],
 
   build: {
