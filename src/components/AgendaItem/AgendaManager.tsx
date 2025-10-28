7139e9fc823cf619dd121d21b3f7e77c1c0e3b3f
Carlos F Arboleda G <arbofercho@gmail.com>
2025-10-27 19:37:10 -0500

    Enable Devfest agenda button

--- src/components/AgendaItem/AgendaManager.tsx
+++ src/components/AgendaItem/AgendaManager.tsx
@@ -38,37 +38,34 @@ export default function AgendaManager({ talks }: AgendaManagerProps) {
 	}
 
 	return (
-		<section className="flex flex-col w-full px-10">
-			<div className="flex flex-row justify-center items-center mx-auto gap-11 w-1/3 h-auto text-sm md:text-base">
-				<button
-					type="button"
-					onClick={() => changeAgendaDay("dataday")}
+    <section className="flex flex-col w-full px-10">
+      <div className="flex flex-row justify-center items-center mx-auto gap-11 w-1/3 h-auto text-sm md:text-base">
+        <button
+          type="button"
+          onClick={() => changeAgendaDay("dataday")}
+          className="flex flex-col py-3 px-6 gap-1  bg-pink-200 hover:bg-[#F5A8CC] min-w-36 md:min-w-52 w-auto cursor-pointer rounded-full "
+        >
+          <span className="font-bold">DataDay</span>
+          <span className="text-xs md:text-sm">7 de noviembre</span>
+        </button>
+        {/* bg-[#CCF6C5] hover:bg-[#BEECA0] */}
+        <button
+          type="button"
+          onClick={() => changeAgendaDay("devfest")}
+          className="flex flex-col py-3 px-6 gap-1 bg-[#EAF7E2] hover:bg-[#BEECA0] min-w-36 md:min-w-52 w-auto cursor-pointer rounded-full"
+        >
+          <span className="font-bold">DevFest</span>
+          <span className="text-xs md:text-sm">8 de noviembre</span>
+        </button>
+      </div>
 
-					className="flex flex-col py-3 px-6 gap-1  bg-pink-200 hover:bg-[#F5A8CC] min-w-36 md:min-w-52 w-auto cursor-pointer rounded-full "
-				>
-					<span className="font-bold">DataDay</span>
-					<span className="text-xs md:text-sm">7 de noviembre</span>
-				</button>
-				{/* bg-[#CCF6C5] hover:bg-[#BEECA0] */}
-				<button
-					type="button"
-					onClick={() => changeAgendaDay("devfest")}
-					className="flex flex-col py-3 px-6 gap-1  bg-[#EAF7E2] min-w-36 md:min-w-52 w-auto cursor-not-allowed rounded-full disable"
-				>
-					<span className="font-bold">DevFest</span>
-					<span className="text-xs md:text-sm">8 de noviembre</span>
-				</button>
-			</div>
-
-
-			<section
-				className={
-					selectedAgendaDay === "dataday" ? oneTrackStyles : twoTrackStyles
-				}
-			>
-				<AgendaDay talks={agendaInfo} eventDay={selectedAgendaDay} />
-
-			</section>
-		</section>
-	);
+      <section
+        className={
+          selectedAgendaDay === "dataday" ? oneTrackStyles : twoTrackStyles
+        }
+      >
+        <AgendaDay talks={agendaInfo} eventDay={selectedAgendaDay} />
+      </section>
+    </section>
+  );
 }
